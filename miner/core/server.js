934a3e9ed7a57066f50d771d751a83823e97f841
QiCuiHub <qi.cui@mail.utoronto.ca>
2019-06-01 04:37:30 -0400

    implement merkle tree next

--- miner/core/server.js
+++ miner/core/server.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const lowdb = require('lowdb');
 const FileAsync = require('lowdb/adapters/FileAsync');
+const {Block, Transaction} = require('./structures.js');
+const crypto = require('crypto');
 
 const app = express();
 
-const bc = new FileAsync('blockchain.json');
-const ut = new FileAsync('utxo.json');
-const blockchain = lowdb(bc);
-const utxodb = lowdb(ut);
+const blockchainDB = new FileAsync('core/blockchain/blockchain.json');
+const utxoDB = new FileAsync('core/blockchain/utxo.json');
+const walletDB = new FileAsync('core/wallet/keys.json');
 
+const blockchain = lowdb(blockchainDB);
+const utxo = lowdb(utxoDB);
+const wallet = lowdb(walletDB);
 
 app.get('/init',
-	(req, res) => {
-		
+	async (req, res) => {	
+
+		// generate wallet key pair
+		let wl = await wallet;
+			
+		let kp = crypto.createECDH('secp256k1');
+		kp.generateKeys();
+	
+		let pri = kp.getPrivateKey('hex');
+		let pub = kp.getPublicKey('hex');
+
+		// save to wallet
+		wl.defaults({0 : {privateKey : pri, publicKey: pub}})
+			.assign({0 : {privateKey : pri, publicKey: pub}})
+			.write()
+
+		// create genesis block transaction
+		let utxo_in = [];
+		let utxo_out = [[pub, 1000]];
+		let transaction = new Transaction(utxo_in, utxo_out); 	
+
+		// create genesis block
+		let txList = [transaction];
+		let prevHash = "000000000000000000000000000000000000000000000000000000000000000";
+		let block = new Block(txList, prevHash);
+
+		// add it to the block chain
+
+
+		//await db.defaults({ 0 : [], user: {}, count: 0 })
+  	//	.write()
+			
+		res.sendStatus(200);
+
+
+	}
+);
+
+app.get('/clear',
+	async (req, res) => {	
+			
 		res.sendStatus(200);
 
 
--- miner/core/structures.js
+++ miner/core/structures.js
@@ -1,24 +1,63 @@
-export class BlockChain{
-	constructor(init){
-		
+const crypto = require('crypto');
+
+//BlockChain = {
+//	
+//
+//}
+
+class Block {
+	constructor() {
+		this.blockHash    = null;
+		this.prevHash     = null;
+		this.txMerkleHash = null;
+  	this.transactions = [];
 	}
 
+	getMerkleRoot() {
+		let txids = this.transactions
+			.map((curr) => {return curr.getID()});
 
-
-}
-
-export class Block{
-
-}
-
-export class Transaction{
-	constructor(){
-		tx_hash = 0;
-		in_utxo = [];
-		out_utxo = [];
+		return txids;
 	}
 }
 
-export class UTXO{
+class Transaction {
+	constructor() {
+		this.input  = [];
+  	this.output = [];
+	}
 
+	getID() {
+		if (this.id === undefined){
+
+			// id is the hash of all inputs and outputs
+			let input_str = this.input
+				.reduce((acc, curr) => {return acc + curr}, "");
+
+			let output_str = this.output
+				.reduce((acc, curr) => {return acc + curr[0] + curr[1]}, "");
+
+			// use sha256
+			let hash = crypto
+				.createHash('sha256')
+				.update(input_str + output_str, 'utf8')
+				.digest('hex');
+
+			return hash;
+
+		}
+		else return this.id;
+
+	}
+
+	repr() {
+		console.log(this.output);
+		return {input : this.input, output: this.output};
+	}
+}
+
+
+module.exports = {
+	Block       : Block,
+	Transaction : Transaction
 }
