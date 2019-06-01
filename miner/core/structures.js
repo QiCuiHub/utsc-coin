c6cb4e3df903af9464452dc0d4e514b1fda76a77
QiCuiHub <qi.cui@mail.utoronto.ca>
2019-06-01 04:40:45 -0400

    refactor

--- miner/core/structures.js
+++ miner/core/structures.js
@@ -1,16 +1,41 @@
 const crypto = require('crypto');
 
-//BlockChain = {
-//	
-//
-//}
+class Transaction {
+	constructor(input, output) {
+		this.input  = input;
+  	this.output = output;
+		this.id     = this.getID();
+	}
+
+	getID() {
+		// id is the hash of all inputs and outputs in order
+		let input_str = this.input
+			.reduce((acc, curr) => {return acc + curr}, "");
+
+		let output_str = this.output
+			.reduce((acc, curr) => {return acc + curr[0] + curr[1]}, "");
+
+		// use sha256
+		let hash = crypto
+			.createHash('sha256')
+			.update(input_str + output_str, 'utf8')
+			.digest('hex');
+
+		return hash;
+	}
+
+	repr() {
+		console.log(this.output);
+		return [input : this.input, output: this.output];
+	}
+}
 
 class Block {
-	constructor() {
-		this.blockHash    = null;
-		this.prevHash     = null;
-		this.txMerkleHash = null;
-  	this.transactions = [];
+	constructor(txList, prevHash) {
+  	this.transactions = txList;
+		this.prevHash     = prevHash;
+		this.txMerkleHash = this.getMerkleRoot();
+		this.blockHash    = "test";
 	}
 
 	getMerkleRoot() {
@@ -21,43 +46,19 @@ class Block {
 	}
 }
 
-class Transaction {
-	constructor() {
-		this.input  = [];
-  	this.output = [];
+class BlockChain {
+	constructor(blockchain){
+		this.blockchain = this.parse(blockchain);
 	}
 
-	getID() {
-		if (this.id === undefined){
+	parse(blockchainJSON){
 
-			// id is the hash of all inputs and outputs
-			let input_str = this.input
-				.reduce((acc, curr) => {return acc + curr}, "");
-
-			let output_str = this.output
-				.reduce((acc, curr) => {return acc + curr[0] + curr[1]}, "");
-
-			// use sha256
-			let hash = crypto
-				.createHash('sha256')
-				.update(input_str + output_str, 'utf8')
-				.digest('hex');
-
-			return hash;
-
-		}
-		else return this.id;
-
-	}
-
-	repr() {
-		console.log(this.output);
-		return {input : this.input, output: this.output};
 	}
 }
 
 
 module.exports = {
+	Transaction : Transaction,
 	Block       : Block,
-	Transaction : Transaction
+	Blockchain  : Blockchain
 }
