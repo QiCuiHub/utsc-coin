52deefca982f00fe0873770c95338b5c340ce949
QiCuiHub <qi.cui@mail.utoronto.ca>
2019-06-16 21:07:25 -0400

    move to miner

--- miner/core/miner.js
+++ miner/core/miner.js
@@ -5,9 +5,25 @@ class Miner {
     this.utxo = blockchain.getUTXOs();
   }
 
-  verifyTX(transaction){
-    
-  }
+	verifyTX(transaction){
+		// txid must equal getID
+		if (transaction.txid != transaction.getID()) 
+			return false
+
+		// payment
+		// signature must be verified
+		if (!ops.verify(transaction.txid, transaction.signature, transaction.publicKey))
+			return false
+
+		// input utxos must be unspent
+			transaction.input.every
+		// input utxos must belong to the pub key
+		// output utxo value must be equal to input utxo value
+
+		// coinbase
+		// txid must be unique
+		// value is _ coins
+	}
 
   verifyBlock(block){
 
--- miner/core/structures.js
+++ miner/core/structures.js
@@ -109,31 +109,6 @@ class Blockchain {
 		this.blocks.push(block);
 	}
 
-	verifyTX(transaction){
-		// txid must equal getID
-		if (transaction.txid != transaction.getID()) 
-			return false
-
-		// payment
-		// signature must be verified
-		if (!ops.verify(transaction.txid, transaction.signature, transaction.publicKey))
-			return false
-
-		// input utxos must be unspent
-			transaction.input.every
-
-		// input utxos must belong to the pub key
-		// output utxo value must be equal to input utxo value
-
-		// coinbase
-		// txid must be unique
-		// value is _ coins
-	}
-
-	verifyBlock(block){
-
-	}
-
 	getUTXOs(){
 		return this.blocks.reduce((acc, block) => {
 			block.transactions.forEach((tx) => {
